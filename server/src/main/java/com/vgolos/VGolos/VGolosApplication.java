8000f6d72f05e0a71f05fa06334359e9e13e20dd
Roman Boiarchuk <r.s.boyarchuk@gmail.com>
2019-05-07 09:51:55 +0300

    initial

---
+++ server/src/main/java/com/vgolos/VGolos/VGolosApplication.java
@@ -0,0 +1,15 @@
+package com.vgolos.VGolos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class VGolosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VGolosApplication.class, args);
+	}
+
+}
