5dc6e219de9df6d36f7fad2b1e2a1dff08df90c8
Roman Boiarchuk <r.s.boyarchuk@gmail.com>
2019-05-10 13:15:53 +0300

    added account service on front

---
+++ client/src/app/model/account.ts
@@ -0,0 +1,6 @@
+export class Account {
+    id : number;
+    login: string;
+    password: string;
+    role: string;
+}
---
+++ client/src/app/service/account.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Empty } from '../model/empty';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+
+  constructor(private http: HttpClient) { }
+  baseUrl: string = 'http://localhost:8080/accounts/';
+
+  findAll(): Observable<Account[]> {
+    return this.http.get<Account[]>(this.baseUrl);
+  }
+
+  findByLogin(login: string): Observable<Account> {
+    return this.http.get<Account>(this.baseUrl + "login/" + login);
+  }
+
+  create(account: Account): Observable<Account> {
+    return this.http.post<Account>(this.baseUrl + "create", account);
+  }
+
+  delete(id: number): Observable<Empty> {
+    return this.http.delete<Empty>(this.baseUrl + "delete/" + id);
+  }
+
+  update(account:Account): Observable<Account>{
+    return this.http.put<Account>(this.baseUrl + "update",account);
+  }
+}
