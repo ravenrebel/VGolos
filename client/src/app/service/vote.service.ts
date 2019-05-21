ab2069d02e7aa4b3422922214c3799e161b49215
ravenrebel <nasty199961@gmail.com>
2019-05-22 01:44:11 +0300

    update vote service on front

--- client/src/app/service/vote.service.ts
+++ client/src/app/service/vote.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Vote } from '../model/vote';
 import { Empty } from '../model/empty';
+import { VoteDTO } from '../model/vote-dto';
 
 
 @Injectable({
@@ -12,19 +13,27 @@ export class VoteService {
   constructor(private http: HttpClient) { }
   baseUrl: string = 'http://localhost:8080/vote/';
 
-  findAll(): Observable<Vote[]> {
-    return this.http.get<Vote[]>(this.baseUrl);
+  findAll(): Observable<VoteDTO[]> {
+    return this.http.get<VoteDTO[]>(this.baseUrl);
   }
-  create(vote:Vote):Observable<Vote> {
-    return this.http.post<Vote>(this.baseUrl+ "create", vote);
+
+  create(vote:Vote):Observable<VoteDTO> {
+    return this.http.post<VoteDTO>(this.baseUrl+ "create", vote);
   }
-  update(vote:Vote): Observable<Vote>{
-    return this.http.put<Vote>(this.baseUrl + "update",vote);
+
+  update(vote:Vote): Observable<VoteDTO>{
+    return this.http.put<VoteDTO>(this.baseUrl + "update",vote);
   }
+
   delete(id: number): Observable<Empty> {
     return this.http.delete<Empty>(this.baseUrl + "delete/" + id);
   }
-  findbylogin(id:number): Observable<Vote>{
-    return this.http.get<Vote>(this.baseUrl+ "id/"+id);
-      }
+
+  findbylogin(id:number): Observable<VoteDTO>{
+    return this.http.get<VoteDTO>(this.baseUrl + "id/" + id);
+  }
+
+  isExisting(electionId:number, citizenId:number): Observable<Boolean>{
+    return this.http.get<Boolean>(this.baseUrl + "isExisting" + electionId + citizenId);
+  }
 }
